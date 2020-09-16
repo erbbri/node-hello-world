5e369d6b67d9b7dc2a410a6919191b3e32a66b39
Bridget <erbb@spu.edu>
2020-09-16 14:55:07 -0700

    Fixed my sins

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const http = require('http');
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer((request, response)) => {
+const server = http.createServer((request, response) => {
         response.statusCode = 200;
         response.setHeader ('Content-Type', 'text/plain');
         response.end('Hello World\n\nCool!');
 });
 
-server.listen(port, err) => {
+server.listen(port, (err) => {
         if(err) {
-                return console.log('Something went wrong ', err);
+                return console.log('Something went terribly wrong ', err);
         }
-        console.log('Server Listening on: ${port}');
+        console.log(`Server Listening on: ${port}`);
 });
